e0238b261d4e4e08254cf7e76204169776d1b27d
1stdomonov <98898208+1stdomonov@users.noreply.github.com>
2023-04-07 12:56:18 -0500

    create topMovies Object

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+//declare, require, and assign EXPRESS
 const express = require('express');
 const app = express();
+
+
+
+//create top 10 movies object
+let topMovies = [
+    {
+      title: 'Brazil',
+      Director: 'Terry Gilliam'
+    },
+    {
+      title: 'Grand Budapest Hotel',
+      Director: 'Wes Anderson'
+    },
+    {
+      title: 'Sound of Metal',
+      Director: 'Darius Marder'
+    },
+    {
+        title: 'Lucky',
+        Director: 'John Carroll Lynch'
+    },
+    {
+        title: 'The Matrix',
+        Director: 'Lilly Wachowski, Lana Wachowski'
+    },
+    {
+        title: 'Lord of the Rings, the fellowship of the ring',
+        Director: 'Peter Jackson'
+    },
+    {
+        title: 'Cyrano de Bergerac',
+        Director: 'Michael Gordon'
+    },
+    {
+        title: 'The Ghost and the Darkness',
+        Director: 'Stephen Hopkins'
+    },
+    {
+        title: 'Jurrasic Park',
+        Director: 'Steven Spielberg'
+    },
+  ];
