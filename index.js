892aa14fcbb5bddc52ecbc30fe1709036ce85793
1stdomonov <98898208+1stdomonov@users.noreply.github.com>
2023-04-07 13:55:40 -0500

    added error handling

--- index.js
+++ index.js
@@ -60,8 +60,10 @@ let topMovies = [
   });
   
 //error handling
-
-
+ app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
   app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
